6b71d2b4f8868eb6d24d81b95071e6c94ea6d1b4
Ivan Sladkov <ivan.sladkov@yandex.ru>
2022-11-23 00:30:16 +0300

    Yet another update to lab.

--- lab/additionalMPI.cpp
+++ lab/additionalMPI.cpp
@@ -21,18 +21,7 @@ int allocate(double ***a, int i, int j) {
 	return 0;
 }
 
-void saveSeq(double **a, int N){
-	FILE *ff = fopen("resultMPI_1a_seq.txt", "w");
-	if (ff == NULL)
-		exit(-2);
-	for (int i=0; i<ISIZE; i++)
-		for (int j = 0; j < JSIZE; j++) {
-			fprintf(ff, "%f", a[i][j]);
-		}
-	fclose(ff);
-}
-
-void save(double **a, int N){
+void save1(double **a){
 	FILE *ff = fopen("resultMPI_1a.txt", "w");
 	if (ff == NULL)
 		exit(-2);
@@ -43,38 +32,38 @@ void save(double **a, int N){
 	fclose(ff);
 }
 
-void save(double **a, int N, bool rewrite) {
-	if (N>-1) return;
-	FILE *ff = fopen("resultMPI_1a.txt", rewrite ? "w" : "a");
+void save2(double **a){
+	FILE *ff = fopen("resultMPI_1a_seq.txt", "w");
 	if (ff == NULL)
 		exit(-2);
-	for (int j = 0; j < JSIZE; j++) {
-		fprintf(ff, "%f", a[N][j]);
-	}
-
+	for (int i=0; i<ISIZE; i++)
+		for (int j = 0; j < JSIZE; j++) {
+			fprintf(ff, "%f", a[i][j]);
+		}
 	fclose(ff);
 }
 
 int main(int argc, char **argv) {
 	MPI_Init(&argc, &argv);
 
-	int rank, procs, ball;
+	int rank, procs;
 	MPI_Comm_size(MPI_COMM_WORLD, &procs);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
 	double **a;
-	if (allocate(&a, ISIZE, JSIZE + 1))
+	if (allocate(&a, ISIZE, JSIZE))
 		return -1;
 	for (int i = 0; i < ISIZE; i++) {
-		for (int j = 0; j < JSIZE + 1; j++) {
+		for (int j = 0; j < JSIZE; j++) {
 			a[i][j] = 10 * i + j;
 		}
 	}
-	
+
 	/*[i-1][j+1]; a[i][JSIZE] inited the same way*/
 
 	double start = MPI_Wtime();
 	MPI_Request r;
+	MPI_Status s;
 	int diag;
 	for (diag=rank; diag<JSIZE; diag+=procs){
 		int j=diag;
@@ -87,59 +76,97 @@ int main(int argc, char **argv) {
 			a[i][j]=sin(2*a[i-1][j+1]);
 		}
 	}
-	for (; diag<JSIZE+ISIZE; diag+=procs){
+	for (++diag; diag<JSIZE+ISIZE; diag+=procs){
 		int j=JSIZE-1;
 		//if (!rank) cout<<"shit1 "<<diag<<endl;
 		for (int i=diag-ISIZE+1; i<ISIZE; i++){
 			j--;
-			
+
 			a[i][j]=sin(2*a[i-1][j+1]);
 		}
 	}
-
-	cout << "Time elapsed: \t" << MPI_Wtime() - start;
-
+	cout<<"Time elapsed for parallel: \t"<<MPI_Wtime()-start<<endl;
 	MPI_Barrier(MPI_COMM_WORLD);
-	ball = 0;
-	if (!rank) {
-		save(a, ball, 1);
-		ball++;
-		MPI_Send(&ball, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
-	}
-	for (int i = rank; i < ISIZE; i += procs) {
-		MPI_Recv(&ball, 1, MPI_INT, rank ? rank - 1 : procs - 1, 0,
-				MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-		if (ball >= ISIZE)
-			break;
-		save(a, ball, 0);
-		ball++;
-		MPI_Send(&ball, 1, MPI_INT, rank + 1 == procs ? 0 : rank + 1, 0,
-				MPI_COMM_WORLD);
-		if (ball >= ISIZE - procs)
-			break;
-	}
 
-	if (rank) {
-		MPI_Finalize();
+
+	if (rank){
+		double *buff = (double*)malloc(sizeof(double)*JSIZE);
+		for (diag=rank; diag<JSIZE; diag+=procs){
+			int j=diag;
+			for (int i=0; i<=diag; i++){
+				buff[j]=a[i][j];
+				j--;
+			}
+			MPI_Send(buff, diag+1, MPI_DOUBLE, 0, diag, MPI_COMM_WORLD);
+		}
+		for (; diag<JSIZE+ISIZE-1; diag+=procs){
+			int j=JSIZE-1;
+			for (int i=diag-ISIZE+1; i<ISIZE; i++){
+
+				buff[j]=a[i][j];
+				j--;
+			}
+			int k=0;
+			for (int i=0; i<2*ISIZE-diag-1; i++){
+				buff[k]=buff[diag+1-ISIZE+i];
+				k++;
+			}
+			MPI_Send(buff, 2*ISIZE-diag-1, MPI_DOUBLE, 0, diag, MPI_COMM_WORLD);
+		}
+	}
+	else{
+		double *buff = (double*)malloc(sizeof(double)*JSIZE);
+		for (diag=0; diag<JSIZE; diag+=1){
+			if (diag%procs==0)
+				continue;
+			MPI_Recv(buff, diag+1, MPI_DOUBLE, MPI_ANY_SOURCE, diag, MPI_COMM_WORLD, &s);
+			int j=diag;
+			for (int i=0; i<=diag; i++){
+				a[i][j]=buff[j];
+				j--;
+			}
+		}
+		for (; diag<JSIZE+ISIZE-1; diag+=1){
+			if (diag%procs==0)
+				continue;
+			MPI_Recv(buff, 2*ISIZE-diag-1, MPI_DOUBLE, MPI_ANY_SOURCE, diag, MPI_COMM_WORLD, &s);
+			int k=ISIZE-1;
+			for (int i=2*ISIZE-diag-2; i>=0; i--){
+				buff[k]=buff[i];
+				k--;
+			}
+			int j=JSIZE-1;
+			for (int i=diag-ISIZE+1; i<ISIZE; i++){
+
+				a[i][j]=buff[j];
+				j--;
+			}
+		}
+	}
+	save1(a);
+
+
+
+	if (rank){
+				MPI_Finalize();
 		return 0;
 	}
-	
 	for (int i = 0; i < ISIZE; i++) {
-		for (int j = 0; j < JSIZE + 1; j++) {
+		for (int j = 0; j < JSIZE; j++) {
 			a[i][j] = 10 * i + j;
 		}
 	}
-	start = MPI_Wtime();
-	for (int i = 0; i < ISIZE; i += 1) {
-		for (int j = 0; j < JSIZE; j++) {
+		start=MPI_Wtime();
+	for (int i = 1; i < ISIZE; i += 1) {
+		for (int j = 0; j < JSIZE-1; j++) {
 			if (i > 0) {
 				a[i][j] = sin(2 * a[i - 1][j + 1]);
 			}
 		}
 	}
-	cout <<endl<< "Time elapsed for sequential: \t" << MPI_Wtime() - start;
-	saveSeq(a, 0);
-	save(a, 0);
-	MPI_Finalize();
+		cout<<"Time elapsed for sequential: \t"<<MPI_Wtime()-start<<endl;
+	save1(a);
+
+		MPI_Finalize();
 	return 0;
 }
